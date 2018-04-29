c7edae6f07abcac5494770b95f449d7d4085caf8
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-04-30 06:33:01 +0800

    Added firebase library and initialisation on App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/comon';
+import firebase from 'firebase';
 
 export default class App extends Component {
+  componentDidMount() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyDf6UWB0FEs4-biGOUW1MCo4dkPqtDF5xY',
+      authDomain: 'rn-authentication-505a8.firebaseapp.com',
+      databaseURL: 'https://rn-authentication-505a8.firebaseio.com',
+      projectId: 'rn-authentication-505a8',
+      storageBucket: 'rn-authentication-505a8.appspot.com',
+      messagingSenderId: '749524062865'
+    };
+    firebase.initializeApp(config);    
+  }
+
   render() {
     return (
       <View>
