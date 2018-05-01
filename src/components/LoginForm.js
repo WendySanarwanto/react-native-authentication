ff983a31443fddf7916a86be08beb47bd441dce0
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-05-02 07:08:44 +0800

    Added spinner progress when authentication is in progress

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,32 +1,52 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 import firebase from 'firebase';
 
 export default class LoginForm extends Component {
-  state = { error: undefined, email: '', password: '' };
+  state = { error: undefined, email: '', password: '', loading: false };
 
   async onClicked() {
     const { email, password } = this.state;
-    this.setState({ error: undefined });
+    this.setState({ error: undefined, loading: true });
 
     try {
-      await firebase.auth()
-        .signInWithEmailAndPassword(email, password);
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      this.onLoginSuccess();
     } catch(signInErr) {
       console.log(`[WARN] - <LoginForm> Sign In using as '${email}' is failing. Details: \n`, signInErr);
       try { 
-        await firebase.auth()
-          .createUserWithEmailAndPassword(email, password);
+        await firebase.auth().createUserWithEmailAndPassword(email, password);
+        this.onLoginSuccess();
       } catch(createUserErr) {
         // Show message
         console.log(`[ERROR] - <LoginForm> Creating a new account with email: '${email}' is failing. Details: \n `, createUserErr);
-        this.setState({error: 'Authentication failed.'});
-      }
+        this.onLoginFail();
+      } 
     }
   }
 
-  render() {
+  onLoginFail() {
+    this.setState({error: 'Authentication failed.', loading: false});
+  }
+
+  onLoginSuccess() {
+    this.setState({ 
+      email: '',
+      password: '',
+      loading: false 
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small"/>
+    }
+
+    return <Button label="Login In" onClicked={ this.onClicked.bind(this) } /> 
+  }
+
+  render() {   
     return (
       <Card>
         <CardSection>
@@ -47,8 +67,7 @@ export default class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button label="Login In"
-            onClicked={ this.onClicked.bind(this) } />
+          { this.renderButton() }
         </CardSection>
       </Card>
     );
