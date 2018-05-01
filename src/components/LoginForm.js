c1d6f00e465dcea7f429748c448aef892324b573
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-05-02 01:08:25 +0800

    implemented Authentication logic

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
+import firebase from 'firebase';
 
 export default class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { error: undefined, email: '', password: '' };
+
+  async onClicked() {
+    const { email, password } = this.state;
+    this.setState({ error: undefined });
+
+    try {
+      await firebase.auth()
+        .signInWithEmailAndPassword(email, password);
+    } catch(signInErr) {
+      console.log(`[WARN] - <LoginForm> Sign In using as '${email}' is failing. Details: \n`, signInErr);
+      try { 
+        await firebase.auth()
+          .createUserWithEmailAndPassword(email, password);
+      } catch(createUserErr) {
+        // Show message
+        console.log(`[ERROR] - <LoginForm> Creating a new account with email: '${email}' is failing. Details: \n `, createUserErr);
+        this.setState({error: 'Authentication failed.'});
+      }
+    }
+  }
 
   render() {
     return (
@@ -20,10 +42,23 @@ export default class LoginForm extends Component {
           onChangeText= { password => this.setState({ password }) } />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          { this.state.error }
+        </Text>
+
         <CardSection>
-          <Button label="Login In" />
+          <Button label="Login In"
+            onClicked={ this.onClicked.bind(this) } />
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
